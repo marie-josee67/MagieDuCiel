9b1a6aa6ba48a6f1964e7de27e02fef81157ec2b
marie-josee <mariejosee89@hotmail.fr>
2025-03-08 14:52:01 +0100

    Build : Découpage de l'index.html + création des functions pour le JS et le CSS.

---
+++ footer.php
@@ -0,0 +1,119 @@
+<!--=========|  Footer Section |=========-->
+<footer class="footer footer-shape footer-shape-home1" id="footer">
+	<div class="footer-wrapper">
+		<div class="container">
+			<div class="footer-widget-area pb-100">
+				<div class="row">
+					<!-- Single Wodget -->
+					<div class="col-lg-4 col-sm-8">
+						<div class="footer-widget footer-widget-about">
+							<h4 class="footer-widget-title">Abias</h4>
+							<div class="footer-widget-content">
+								<p>We are absolute kopa sa company, if you have any doute please samne is ese kotha bol, pagla molom lagaibo.</p>
+								<ul class="footer-widget-social">
+									<li><a target="_blank" class="icofont-facebook" href="#"></a></li>
+									<li><a target="_blank" class="icofont-vimeo" href="#"></a></li>
+									<li><a target="_blank" class="icofont-twitter" href="#"></a></li>
+									<li><a target="_blank" class="icofont-pinterest" href="#"></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<!-- Single Wodget -->
+					<div class="col-lg-2 col-sm-4">
+						<div class="footer-widget">
+							<h4 class="footer-widget-title">Services</h4>
+							<div class="footer-widget-content">
+								<ul class="footer-list">
+									<li><a href="single-service.html">Graphics Design</a></li>
+									<li><a href="single-service.html">UI Design</a></li>
+									<li><a href="single-service.html">Web Design</a></li>
+									<li><a href="single-service.html">Web Development</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<!-- Single Wodget -->
+					<div class="col-lg-2 col-sm-4">
+						<div class="footer-widget">
+							<h4 class="footer-widget-title">Pages</h4>
+							<div class="footer-widget-content">
+								<ul class="footer-list">
+									<li><a href="index.html">Home</a></li>
+									<li><a href="about-me.html">About</a></li>
+									<li><a href="service_2.html">Service</a></li>
+									<li><a href="portfolio_1.html">Portfolio</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<!-- Single Wodget -->
+					<div class="col-lg-2 col-sm-4">
+						<div class="footer-widget">
+							<h4 class="footer-widget-title">Quick Links</h4>
+							<div class="footer-widget-content">
+								<ul class="footer-list">
+									<li><a href="blog.html">Blog Page</a></li>
+									<li><a href="single.html">Blog Single</a></li>
+									<li><a href="single-service.html">Service Details</a></li>
+									<li><a href="single-project.html">Project Details</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<!-- Single Wodget -->
+					<div class="col-lg-2 col-sm-4">
+						<div class="footer-widget">
+							<h4 class="footer-widget-title">Support</h4>
+							<div class="footer-widget-content">
+								<ul class="footer-list">
+									<li><a href="contact.html">Contact</a></li>
+									<li><a href="faq.html">FAQ</a></li>
+									<li><a href="contact.html">Complain</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- / Footer Widget area end-->
+			<div class="copyright">
+				<p>&copy; 2021 <a target="_blank" href="https://themeforest.net/user/webglib">webglib</a>. All rights reserved. </p>
+			</div>
+		</div>
+	</div>
+</footer>
+<!--=========| / Footer Section |=========-->
+
+<!--=====| Back To Top |=====-->
+<div class="scroll-top">
+	<button>
+		<i class="icofont-simple-up"></i>
+	</button>
+</div>
+
+<!-- All js file -->
+	<script src="assets/js/jquery-3.5.1.min.js"></script>
+	<!-- Bootstrap js -->
+	<script src="assets/js/popper.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<!-- js for plugin install -->
+	<script src="assets/js/modernizr.js"></script>
+	<script src="assets/js/isotope.pkgd.min.js"></script>
+	<script src="assets/js/imagesloaded.pkgd.min.js"></script>
+	<script src="assets/js/jquery.waypoints.min.js"></script>
+	<script src="assets/js/animated-counter.js"></script>
+	<script src="assets/js/jquery.meanmenu.min.js"></script>
+	<script src="assets/js/parallax.min.js"></script>
+	<script src="assets/js/animated-headline.js"></script>
+	<script src="assets/js/one-page-nav.js"></script>
+	<script src="assets/js/slick.min.js"></script>
+	<script src="assets/js/particles.min.js"></script>
+	<script src="assets/js/tilt.jquery.min.js"></script>
+	<script src="assets/js/jquery.pointlight.js"></script>
+	<script src="assets/js/main.js"></script>
+
+    <!-- appel la function footer -->
+   <?php wp_footer(); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,94 @@
+<!DOCTYPE HTML>
+<html class="no-js" lang="en">
+<head>
+	<!-- Required Meta Tags -->
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- appel la function head -->
+    <?php wp_head(); ?>
+</head>
+<body>
+<!--=========| Preloader Start |=========-->
+<div id="ftco-loader" class="show fullscreen pre-loader">
+	<div class="loader-position">
+		<img src="assets/img/preloader.gif" alt="Preloader">
+	</div>
+</div>
+<!--=========| Preloader End   |=========-->
+<!--=========| Header Section |=========-->
+<header>
+	<!--  navigation start -->
+	<div class="stickynav nav-full-bg stickynav-dark" id="stickynav">
+		<div class="container">
+			<div class="navigation-area">
+				<div class="logo">
+					<a href="#" class="navbar-brand">
+						<img class="logo-dark" src="assets/img/logo/logo_dark.png" alt="Abias logo">
+						<img class="logo-white" src="assets/img/logo/logo.png" alt="Abias logo">
+					</a>
+				</div>
+				<div class="main-menu main-menu-dark ml-auto">
+					<nav class="menu" id="mobile-menu">
+						<ul class="menu-list" id="nav">
+							<li class="active"><a href="#home">Home</a>
+								<ul class="sub-menu">
+									<li><a href="index.html">Home 1</a></li>
+									<li><a href="home_2.html">Home 2</a></li>
+									<li><a href="home_3.html">Home 3</a></li>
+									<li><a href="home_dentist.html">Home 4</a></li>
+									<li><a href="home_dark.html">Home Dark</a></li>
+									<li><a href="home_minimal.html">Home Minimal</a></li>
+									<li><a href="grid_port.html">Grid Portfolio</a></li>
+									<li><a href="creative.html">Creative Portfolio</a></li>
+									<li><a href="creative_2.html">Creative Portfolio 2</a></li>
+								</ul>
+							</li>
+							<li><a href="about-me.html">About</a>
+								<ul class="sub-menu">
+									<li><a href="about-me.html">About Me</a></li>
+									<li><a href="about-us.html">About Us</a></li>
+								</ul>
+							</li>
+							<li><a href="service_2.html">Service</a>
+								<ul class="sub-menu">
+									<li><a href="service_1.html">Service 1</a></li>
+									<li><a href="service_2.html">Service 2</a></li>
+									<li><a href="single-service.html">Service Details</a></li>
+								</ul>
+							</li>
+							<li><a href="portfolio_1.html">Portfolio</a>
+								<ul class="sub-menu">
+									<li><a href="portfolio_1.html">Portolio 1</a></li>
+									<li><a href="portfolio_2.html">Portolio 2</a></li>
+									<li><a href="portfolio_3.html">Portolio 3</a></li>
+									<li><a href="single-project.html">Portolio Details</a></li>
+								</ul>
+							</li>
+							<li><a href="#page">Pages</a>
+								<ul class="sub-menu">
+									<li><a href="404.html">404 Page</a></li>
+									<li><a href="coming.html">Coming Soon</a></li>
+									<li><a href="pricing.html">Pricing Page</a></li>
+									<li><a href="faq.html">Faq Page</a></li>
+								</ul>
+							</li>
+							<li><a href="blog.html">Blog</a>
+								<ul class="sub-menu">
+									<li><a href="blog.html">Blog 2 Column</a></li>
+									<li><a href="blog_3.html">Blog 3 Column</a></li>
+									<li><a href="single.html">Blog Details</a></li>
+								</ul>
+							</li>
+							<li><a href="contact.html">Contact</a></li>
+						</ul>
+					</nav>
+				</div>
+				<div class="mobile-menu mobile-menu-dark"></div>
+			</div>
+		</div>
+	</div>
+	<!-- navigation end -->
+</header>
+<!--=========| / Header Section |=========-->

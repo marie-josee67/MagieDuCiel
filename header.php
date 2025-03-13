ee0ffb3ae9f32385fb50b309f45807c71624c357
John Doe <mariejosee89@hotmail.fr>
2025-03-13 08:18:15 +0100

    test

---
+++ header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE HTML>
+<html class="no-js" lang="en">
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+</head>
+<body>
+<!--=========| Preloader Start |=========-->
+<div id="ftco-loader" class="show fullscreen pre-loader">
+	<div class="loader-position">
+		<img src="assets/img/preloader.gif" alt="Preloader">
+	</div>
+</div>
+<!--=========| Preloader End   |=========-->
+<!--=========| Header Section |=========-->
+<header>
+	<!--  navigation start -->
+	<div class="stickynav nav-full-bg stickynav-dark" id="stickynav">
+		<div class="container">
+			<div class="navigation-area">
+				<div class="logo">
+					<a href="#" class="navbar-brand">
+						<img class="logo-dark" src="assets/img/logo/logo_dark.png" alt="Abias logo">
+						<img class="logo-white" src="assets/img/logo/logo.png" alt="Abias logo">
+					</a>
+				</div>
+				<div class="main-menu main-menu-dark ml-auto">
+					<nav class="menu" id="mobile-menu">
+						<ul class="menu-list" id="nav">
+							<li class="active"><a href="home_dark.html">Accueil</a>
+							</li>
+							<li><a href="about-us.html">A propos</a>
+							</li>
+							<li><a href="service_2.html">Services</a>
+								<ul class="sub-menu">
+									<li><a href="single-service.html">Service Details</a></li>
+								</ul>
+							</li>
+							<li><a href="#page">Pages</a>
+								<ul class="sub-menu">
+									<li><a href="404.html">404 Page</a></li>
+								</ul>
+							</li>
+							<li><a href="contact.html">Contact</a></li>
+						</ul>
+					</nav>
+				</div>
+				<div class="mobile-menu mobile-menu-dark"></div>
+			</div>
+		</div>
+	</div>
+	<!-- navigation end -->
+</header>
+<!--=========| / Header Section |=========-->

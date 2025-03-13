27963cd0d95a7b7cd96eb7d77a5668e2dafda37e
John Doe <mariejosee89@hotmail.fr>
2025-03-13 08:17:11 +0100

    Récupération de base

---
+++ footer.php
@@ -0,0 +1,43 @@
+<!--=========|  Footer Section |=========-->
+<footer class="footer footer-shape footer-shape-home1" id="footer">
+	<div class="footer-wrapper">
+		<div class="container">
+			<div class="footer-widget-area pb-100">
+				<div class="row">
+					<!-- Single Wodget -->
+					<div class="col-lg-4 col-sm-8">
+						<div class="footer-widget footer-widget-about">
+							<h4 class="footer-widget-title">Abias</h4>
+							<div class="footer-widget-content">
+								<p>We are absolute kopa sa company, if you have any doute please samne is ese kotha bol, pagla molom lagaibo.</p>
+								<ul class="footer-widget-social">
+									<li><a target="_blank" class="icofont-facebook" href="#"></a></li>
+									<li><a target="_blank" class="icofont-vimeo" href="#"></a></li>
+									<li><a target="_blank" class="icofont-twitter" href="#"></a></li>
+									<li><a target="_blank" class="icofont-pinterest" href="#"></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>	
+				</div>
+			</div>
+			<!-- / Footer Widget area end-->
+			<div class="copyright">
+				<p>&copy; 2025 <a target="_blank" href="https://themeforest.net/user/webglib">webglib</a>. All rights reserved. </p>
+			</div>
+		</div>
+	</div>
+</footer>
+<!--=========| / Footer Section |=========-->
+
+<!--=====| Back To Top |=====-->
+<div class="scroll-top">
+	<button>
+		<i class="icofont-simple-up"></i>
+	</button>
+</div>
+<?php 
+    wp_footer();
+ ?>
+</body>
+</html>
